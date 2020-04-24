e6a7f455a0d0936fd07654533ef2c3b39ebb6ccf
Pickle Rick <jhell85@gmail.com>
2020-04-24 14:20:13 -0700

    edited response from api call

--- src/currency_exchanger.js
+++ src/currency_exchanger.js
@@ -2,21 +2,22 @@
 /* eslint-disable no-console */
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
+let currencyFormat = require("currency-format");
 
 export class CEService {
-  async getExchangeRates(){
-    try{
-      let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/USD`);
-      console.log(response);
-      
+  async getExchangeRates(currency) {
+    try {
+      let response = await fetch(
+        `https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/${currency}`
+      );
       let jsonifiedResponse;
-      if(response.ok && response.status == 200) {
+      if (response.ok && response.status == 200) {
         jsonifiedResponse = await response.json();
       } else {
         jsonifiedResponse = false;
       }
       return jsonifiedResponse;
-    } catch (error){
+    } catch (error) {
       return false;
     }
   }
