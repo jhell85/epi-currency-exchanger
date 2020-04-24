0f07e74ec0a037a5a010d02371b04b06e8abc2ec
Pickle Rick <jhell85@gmail.com>
2020-04-24 14:22:08 -0700

    added values for currency exchange

--- src/main.js
+++ src/main.js
@@ -8,15 +8,33 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 import $ from "jquery";
 import { CEService } from "./currency_exchanger";
+let currencyFormat = require("currency-format");
 
 $(document).ready(function () {
   $("#submitBtn").click(function () {
-    
+    let currency = $("#currency").val();
+    let amount = $("#amount").val();
     (async () => {
-      console.log("in");
       let currencyData = new CEService();
-      const response = await currencyData.getExchangeRates();
+      const response = await currencyData.getExchangeRates(currency);
       console.log(response);
     })();
   });
+  populateDropDown();
 });
+
+function populateDropDown() {
+  console.log(currencyFormat);
+
+  let currencyCodes = ["USD", "AED", "ARS", "AUD", "BGN", "BRL", "BSD", "CAD", "CHF", "CLP", "CNY", "COP", "CZK", "DKK", "DOP", "EGP", "EUR", "FJD", "GBP", "GTQ", "HKD", "HRK", "HUF", "IDR", "ILS", "INR", "ISK", "JPY", "KRW", "KZT", "MXN", "MYR", "NOK", "NZD", "PAB", "PEN", "PHP", "PKR", "PLN", "PYG", "RON", "RUB", "SAR", "SEK", "SGD", "THB", "TRY", "TWD", "UAH", "UYU", "ZAR"]
+  console.log(currencyCodes.length);
+  
+  currencyCodes.forEach((code) => {
+    for (const key in currencyFormat)
+      if (code === key)
+        // console.log(currencyFormat[key]['name']);
+        $("#currency").append(
+          `<option value="${key}">${currencyFormat[key]["name"]}</option>`
+        );
+  });
+}
