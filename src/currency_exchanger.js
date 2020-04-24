6400388a37d78fb450cf39e5f70703e26953b402
Pickle Rick <jhell85@gmail.com>
2020-04-24 09:47:22 -0700

    added api call but won't call properly

--- src/currency_exchanger.js
+++ src/currency_exchanger.js
@@ -1,7 +1,21 @@
+/* eslint-disable indent */
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+
 export class CEService {
   async getExchangeRates(){
     try{
-      let response = await fetch(`https://prime.exchangerate-api.com/v5/API_KEY/latest/USD`)
+      let response = await fetch(`https://prime.exchangerate-api.com/v5/API_KEY/latest/USD`);
+      let jsonifiedResponse;
+      if(response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
+    } catch (error){
+      return false;
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
+/* eslint-disable indent */
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
 import $ from "jquery";
+import { CEService } from "./currency_exchanger";
+
+$(document).ready(function () {
+  $("#submitBtn").click(function () {
+    
+    async () => {
+      console.log("in");
+      let currencyData = new CEService();
+      const response = await currencyData.getExchangeRates();
+      console.log(response);
+    };
+  });
+});
