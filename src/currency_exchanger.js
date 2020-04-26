b6bab57edcb4297303db5903dd944f7bc085ae99
Pickle Rick <jhell85@gmail.com>
2020-04-25 21:29:33 -0700

    formatted document and removed unnecessary line breaks

--- src/currency_exchanger.js
+++ src/currency_exchanger.js
@@ -24,19 +24,19 @@ export class CEService {
 }
 
 export class CurrencyExchange {
-  constructor(currency, amount, data){
+  constructor(currency, amount, data) {
     this.currencyCode = currency;
     this.amount = amount;
     this.data = data;
   }
-  getCurrencyData(){
-    for (const key in currencyFormat){
-      if (key === this.currencyCode){
-        this.name = currencyFormat[key]['name']
-        this.symbol = currencyFormat[key]['symbol']['grapheme']
+  getCurrencyData() {
+    for (const key in currencyFormat) {
+      if (key === this.currencyCode) {
+        this.name = currencyFormat[key]["name"];
+        this.symbol = currencyFormat[key]["symbol"]["grapheme"];
         console.log(`name ${this.name} symbol ${this.symbol}`);
-        
-        if(currencyFormat[key]['symbol']['template'] === '1 $' ){
+
+        if (currencyFormat[key]["symbol"]["template"] === "1 $") {
           this.inFront = false;
         } else {
           this.inFront = true;
@@ -44,20 +44,24 @@ export class CurrencyExchange {
       }
     }
   }
-  convert(){
-    let header = `<h3 class="text-center mt-3 mb-3">${this.inFront ? this.symbol : ""}${this.amount}${this.inFront ? '': this.symbol} ${this.name} is trading at </h3>`
+  convert() {
+    let header = `<div class="row"><div class="col-md-6 offset-md-3 output"><h2 class="text-center">${this.inFront ? this.symbol : ""}${this.amount}${this.inFront ? '': this.symbol} ${this.name} is trading at</h2></div></div> `;
     let printList = [];
-    let rates = this.data["conversion_rates"]
-    for (const code in rates){
-      for(const key in currencyFormat)
-      if(code === key){
-        let convertedAmount = Math.round((rates[code] * this.amount) * 100)/100
-        printList.push(`<div class="">${convertedAmount} ${currencyFormat[key]['name']}</div>`);
-      }
+    let rates = this.data["conversion_rates"];
+    for (const code in rates) {
+      for (const key in currencyFormat)
+        if (code === key) {
+          let convertedAmount =
+            Math.round(rates[code] * this.amount * 100) / 100;
+          printList.push(
+            `<div>${convertedAmount} ${currencyFormat[key]["name"]}</div>`
+          );
+        }
     }
-    printList.shift()
-    printList.unshift(header)
+    printList.shift();
+    printList.unshift(header);
     console.log(printList);
-    return printList
+
+    return printList;
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,3 @@
-/* eslint-disable indent */
-/* eslint-disable no-console */
-/* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
-
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
@@ -17,45 +12,46 @@ $(document).ready(function () {
       let currency = $("#currency").val();
       let currencyData = new CEService();
       const response = await currencyData.getExchangeRates(currency);
-      console.log(response);
-      let currencyExchange = new CurrencyExchange(currency, amount, response);
-      currencyExchange.getCurrencyData();
-      let displayList = currencyExchange.convert();
-      let header = displayList.shift()
-      $('#output-container').html(`${header}<div class="output row justify-content-md-center"></div>`)
-      $('.output').html(printList(displayList));
-      
+      if (response.error) {
+        $('#output-container').html(`<div class="row"><div class="col-md-6 offset-md-3 output"><h2 class="text-center">Sorry something went wrong!!</h2></div></div><div class="row justify-content-md-center"><div class=""><img class="rounded error"src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fthumbs.dreamstime.com%2Fb%2Ferror-like-laptop-dead-emoji-icon-symbol-page-not-found-web-site-under-construction-maintenance-concept-cartoon-134304720.jpg&f=1&nofb=1"</div></div>`);
+      } else {
+        let currencyExchange = new CurrencyExchange(currency, amount, response);
+        currencyExchange.getCurrencyData();
+        let displayList = currencyExchange.convert();
+        let header = displayList.shift();
+        $("#output-container").html(
+          `${header}<div id="output" class="output pt-3 pl-5 row justify-content-md-around"></div>`
+        );
+        $("#output").html(printList(displayList));
+      }
     })();
-
   });
   populateDropDown();
 });
 
-function printList(displayList){
-  let display = ""
-  display += `<div class="col-3">`
+function printList(displayList) {
+  let display = "";
+  display += `<div class="col-3">`;
   for (let i = 0; i < displayList.length; i++) {
     const element = displayList[i];
-     if (i % 10 === 0 && i != 1 && i != 0){
-      display += `</div><div class="col-3 pb-3">${element}`
+    if (i % 5 === 0 && i != 1 && i != 0) {
+      display += `</div><div class="col-3 pb-1 font-weight-normal">${element}`;
     } else {
-      display += element
+      display += element;
     }
   }
-  return display
+  return display;
 }
 
 function populateDropDown() {
-  console.log(currencyFormat);
-
   let currencyCodes = ["USD", "AED", "ARS", "AUD", "BGN", "BRL", "BSD", "CAD", "CHF", "CLP", "CNY", "COP", "CZK", "DKK", "DOP", "EGP", "EUR", "FJD", "GBP", "GTQ", "HKD", "HRK", "HUF", "IDR", "ILS", "INR", "ISK", "JPY", "KRW", "KZT", "MXN", "MYR", "NOK", "NZD", "PAB", "PEN", "PHP", "PKR", "PLN", "PYG", "RON", "RUB", "SAR", "SEK", "SGD", "THB", "TRY", "TWD", "UAH", "UYU", "ZAR"];
   currencyCodes.forEach((code) => {
     for (const key in currencyFormat)
       if (code === key)
+        // the line of console.log below is uncomment it will not let the dropdown list populate properly, but Why?
         // console.log(currencyFormat[key]['name']);
         $("#currency").append(
           `<option value="${key}">${currencyFormat[key]["name"]}</option>`
         );
   });
 }
-
